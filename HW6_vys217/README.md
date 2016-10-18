f752a6a1b93091ba5d502480716b220d9e81f5fc
Vishwajeet Shelar <vys217@compute.cusp.nyu.edu>
2016-10-18 17:04:43 -0400

    HW6

---
+++ HW6_vys217/README.md
@@ -0,0 +1,16 @@
+# Collaborators: I worked with Aaron Dsouza, Achilles Saxby, Priyanshi Singh, Anastasia Shegay and Akshay Penmatcha for this Homework.
+
+## Assignment 1:
+
+I cleaned the data and used references from the internet to work on this assignment.
+
+I referred the Applied Data Science code for getting the ols function without creating a column of ones.
+
+Most of my work was done individually and I assisted my collaborators with their assignment.
+
+## Assignment 2:
+
+I helped Aaron with the programming in this assignment. I also proofread the Authorea paper with was majorly written by Achilles Saxby.
+
+The paper can be found on https://www.authorea.com/users/106123/articles/133483/_show_article
+
--- HW6_vys217/getcsv.py
+++ HW6_vys217/getcsv.py
@@ -30,13 +30,13 @@ def getEnergycsv():
         
 
 def getPLUTOcsv():
-    '''The function downloads the 'Emappluto_16v1.zip'
+    '''The function downloads the 'mappluto_16v1.zip'
     Author: vys217
     '''
     print ("Downloading")
     ### First I will heck that it is not already there
-    if not os.path.isfile(os.getenv("PUIDATA") + "/" + "Manhattan/"):
-        if os.path.isfile("Manhattan/"):
+    if not os.path.isfile(os.getenv("PUIDATA") + "/" + "Manhattan/MNMapPLUTO.shp"):
+        if os.path.isfile("Manhattan/MNMapPLUTO.shp"):
             # if in the current dir just move it
             if os.system("mv " + "Manhattan " + os.getenv("PUIDATA")):
                 print ("Error moving file!, Please check!")
@@ -48,7 +48,7 @@ def getPLUTOcsv():
                 ###  To move it I use the os.system() functions to run bash commands with arguments
                 os.system("mv " + "mappluto_16v1.zip " + os.getenv("PUIDATA"))
             ### unzip the csv
-            if os.system("unzip " + os.getenv("PUIDATA") + "/" + "mappluto_16v1.zip"):
+            if not os.system("unzip " + os.getenv("PUIDATA") + "/" + "mappluto_16v1.zip"):
                 print("Unzipped")
                 os.system("mv " + "Manhattan " + os.getenv("PUIDATA"))
                 os.system("rm -r Bronx/")
